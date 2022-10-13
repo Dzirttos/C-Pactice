9e1d9f79500b9bb6fd9a5a7d10a73f498dd0ada1
Dizrtt <demonic2003@bk.ru>
2022-10-13 20:47:21 +0200

    More variants of squaring added

--- PJ1/Program.cs
+++ PJ1/Program.cs
@@ -6,6 +6,9 @@ Console.Write("Enter the number: ");
 
 int number = Convert.ToInt32(Console.ReadLine());
 
+
 int sqr = number * number;
+// или number *= number;
+// или int sqr1 Convert.ToInt32(Math.Pow(number, 2));
 
 Console.WriteLine("The new number: " + sqr); 
