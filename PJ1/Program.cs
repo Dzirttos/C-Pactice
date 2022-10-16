4d0ca0c07c1eacb80a593b8431c4f7ce90279a95
Dizrtt <demonic2003@bk.ru>
2022-10-16 12:19:40 +0200

    Minor changes added

--- PJ1/Program.cs
+++ PJ1/Program.cs
@@ -9,6 +9,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int sqr = number * number;
 // или number *= number;
-// или int sqr1 Convert.ToInt32(Math.Pow(number, 2));
+// или int = sqr1 Convert.ToInt32(Math.Pow(number, 2));
 
 Console.WriteLine("The new number: " + sqr); 
