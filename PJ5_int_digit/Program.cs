e2ce32b3ab4e9244cf8acc21b1423a36e64c7a33
Dizrtt <demonic2003@bk.ru>
2022-10-16 13:32:58 +0200

    Digit interval task comleted

--- PJ5_int_digit/Program.cs
+++ PJ5_int_digit/Program.cs
@@ -1,3 +1,29 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от - N до N 
 Console.Clear();
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int negativenumber = -number;
+
+if (number == 0)
+{
+    Console.WriteLine("Введён 0, интервал отсутствует");
+}
+if (number > 0)
+{
+    
+    while (negativenumber <= number)
+    {
+    Console.Write(negativenumber + " ");
+    negativenumber++;
+    }
+}
+else
+{
+       while (negativenumber >= number)
+    {
+    Console.Write(number + " ");
+    number++;
+    } 
+}
