48b6a16235b84863907ebb6e287d2ac0b36386b3
Dizrtt <demonic2003@bk.ru>
2022-10-18 00:53:12 +0200

    First lab task completed

---
+++ LAB/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Генерируется случайное число n в интервале [20 - 99]. 
+// Проверяется делится ли оно на  введенное с клавиатуры целое число d (используйте тернарный оператор)
+// Console.Clear();
+
+// Console.Write("Enter number: ");
+// int d = Convert.ToInt32(Console.ReadLine());
+
+// int n = new Random().Next(20, 99);
+// int eq = n % d;
+// Console.WriteLine(n);
+// Console.WriteLine(d);
+// Console.WriteLine(eq == 0 ? "Делится" : "не делится");
+
