fe50cba7b1740e18ba13da64dd4fb3ebe65dbbf4
Dizrtt <demonic2003@bk.ru>
2022-10-18 20:38:34 +0200

    Tasks 2-4 solved and added

--- LAB/Program.cs
+++ LAB/Program.cs
@@ -5,9 +5,77 @@
 // Console.Write("Enter number: ");
 // int d = Convert.ToInt32(Console.ReadLine());
 
-// int n = new Random().Next(20, 99);
+// int n = new Random().Next(20, 100);
 // int eq = n % d;
 // Console.WriteLine(n);
 // Console.WriteLine(d);
 // Console.WriteLine(eq == 0 ? "Делится" : "не делится");
 
+// 2. Генерируется два случайных числа a и b и интервале [10 - 30]. Если a>b, выводится их разность, если a<b, то выводится сумма, 
+// если a==b выводится собщение "числа равны".
+
+// Console.Clear();
+
+// int a = new Random().Next(10, 31);
+// int b = new Random().Next(10, 31);
+
+// if (a > b)
+// {
+//     Console.WriteLine($"{a} минуc {b} = {a - b}");
+// }
+// else if (a == b)
+// {
+//     Console.WriteLine($"{a} равно {b}");
+// }
+// else
+//     Console.WriteLine($"{a} плюс {b} = {a + b}");
+
+// 3. Генерируется два случайных числа c и d в интервале [- 20 - 99]. 
+// Если оба чётные, выводится их частное, если оба нечётные - сумма, ели одно четное, одно нет, вывводится их произведение.
+
+// Console.Clear();
+
+// double c = new Random().Next(-10, 100);
+// double d = new Random().Next(-10, 100);
+
+// if (c%2==0 && d%2==0)
+// {
+//     Console.WriteLine($"{c} деленное на {d} = {c/d}");
+// }
+// else if (c%2!=0 && d%2!=0)
+// {
+//     Console.WriteLine($"{c} плюс {d} = {c+d}");
+// }
+// else
+// {
+//     Console.WriteLine($"{c} умножить на {d} = {c*d}");
+// }
+
+
+//4. По введенному номеру месяца выводит название поры года (зима, весна, лето, осень) и сообщение "сессия, каникулы, 1 семестр, 2 семестр"
+// Console.Clear();
+
+// string mes;
+// Console.WriteLine("Введите номер месяца: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:");
+// string month = Console.ReadLine();
+
+// switch (month)
+// {
+//     case "1": mes = "Зима. Сессия"; break;
+//     case "2": mes = "Зима. 2 Семестр"; break;
+//     case "3": mes = "Весна. 2 Семестр"; break;
+//     case "4": mes = "Весна. 2 Семестр"; break;
+//     case "5": mes = "Весна. 2 Семестр"; break;
+//     case "6": mes = "Лето. Сессия"; break;
+//     case "7": mes = "Лето. Каникулы"; break;
+//     case "8": mes = "Лето. Каникулы"; break;
+//     case "9": mes = "Осень. 1 Семестр"; break;
+//     case "10": mes = "Осень. 1 Семестр"; break;
+//     case "11": mes = "Осень. 1 Семестр"; break;
+//     case "12": mes = "Зима. 1 Семестр"; break;
+//     default: mes = "Номер месяца введен некорректно"; break;
+// }
+// Console.WriteLine(mes);
+
+//5. По введенному номеру месяца выводит название поры года (зима, весна, лето, осень) и сообщение "сессия, каникулы, 1 семестр, 2 семестр"
+// Console.Clear();
