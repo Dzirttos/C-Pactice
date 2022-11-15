970a0214a4d9cea4d4abc6f30bc3c068ed87ac2a
Dizrtt <demonic2003@bk.ru>
2022-11-15 23:17:31 +0200

    Class library added to make a refference for premade functions

---
+++ Class/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Class;
+public class func
+{
+   // создает целочисленный массив длинной size с числами от start до end
+    public static int[] CreateRandomArray(int N, int start, int end)
+    {
+
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+    }
+
+// выводит массив на экран через пробел
+    public static void ShowArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
